d34eeee846a99dbb749f999633433d3f2c97ea01
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2023-10-09 13:44:43 -0700

    Archivos iniciales de creación de proyecto.

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DinoAprende_Residencia_Profesional"
+include(":app")
+ 
