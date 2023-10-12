6c3416c01ee3966bc049d2b89e78013b5a9a221c
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2023-10-12 13:46:36 -0700

    Backend para formulario del Feedback, listo. Agregar alerta de confirmación.

--- app/src/main/java/com/example/dinoaprende_residencia_profesional/FeedbackForm.java
+++ app/src/main/java/com/example/dinoaprende_residencia_profesional/FeedbackForm.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class FeedbackForm extends AppCompatActivity {
 
@@ -26,12 +32,49 @@ public class FeedbackForm extends AppCompatActivity {
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showSuccessDialog();
+                showExitDialog();
+            }
+        });
+
+        RadioButton radioButton1 = findViewById(R.id.option1Q1);
+        RadioButton radioButton2 = findViewById(R.id.option2Q1);
+        RadioButton radioButton3 = findViewById(R.id.option3Q1);
+        EditText editText = findViewById(R.id.txtComment);
+        Button btnSend = findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("message/html");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"soportedinoaprende@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Opinión sobre DinoAprende.");
+
+                if (radioButton1.isChecked()){
+                    intent.putExtra(Intent.EXTRA_TEXT, "¿A esta persona le agradan los juegos que ofrecemos?: Si, y mucho." +
+                            "\n\n Su opinión sobre la aplicación: " + editText.getText());
+                }
+                else if (radioButton2.isChecked()){
+                    intent.putExtra(Intent.EXTRA_TEXT, "¿A esta persona le agradan los juegos que ofrecemos?: Solo un poco." +
+                            "\n\n Su opinión sobre la aplicación: " + editText.getText());
+                }
+                else if (radioButton3.isChecked()){
+                    intent.putExtra(Intent.EXTRA_TEXT, "¿A esta persona le agradan los juegos que ofrecemos?: Para nada..." +
+                            "\n\n Su opinión sobre la aplicación: " + editText.getText());
+                }
+
+
+
+                try {
+                    startActivity(Intent.createChooser(intent, "Porfavor, selecciona un correo..."));
+                } catch (android.content.ActivityNotFoundException exception) {
+                    Toast.makeText(FeedbackForm.this, "No se encontró ningún correo...", Toast.LENGTH_SHORT);
+                }
             }
         });
     }
 
-    private void showSuccessDialog(){
+    private void showExitDialog(){
         ConstraintLayout dialogConstraintLayout = findViewById(R.id.layoutDialog);
         View view = LayoutInflater.from(FeedbackForm.this).inflate(R.layout.dialog_custom1, dialogConstraintLayout);
         Button btnUnderstand = view.findViewById(R.id.btnUnderstand);
