2805e4e499dd2f18f969ec9e95ad4147bd4f17bb
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2023-10-12 12:57:29 -0700

    Diseño de la alerta personalizada 1, terminado.

--- app/src/main/java/com/example/dinoaprende_residencia_profesional/FeedbackForm.java
+++ app/src/main/java/com/example/dinoaprende_residencia_profesional/FeedbackForm.java
@@ -1,11 +1,16 @@
 package com.example.dinoaprende_residencia_profesional;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class FeedbackForm extends AppCompatActivity {
@@ -21,8 +26,38 @@ public class FeedbackForm extends AppCompatActivity {
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                showSuccessDialog();
+            }
+        });
+    }
+
+    private void showSuccessDialog(){
+        ConstraintLayout dialogConstraintLayout = findViewById(R.id.layoutDialog);
+        View view = LayoutInflater.from(FeedbackForm.this).inflate(R.layout.dialog_custom1, dialogConstraintLayout);
+        Button btnUnderstand = view.findViewById(R.id.btnUnderstand);
+        Button btnCancel = view.findViewById(R.id.btnCancel);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(FeedbackForm.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        btnUnderstand.findViewById(R.id.btnUnderstand).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 finish();
             }
         });
+
+        btnCancel.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        if (alertDialog.getWindow() != null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
     }
 }
